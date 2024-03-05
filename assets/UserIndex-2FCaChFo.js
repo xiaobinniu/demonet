c9a306798ecdda74bc4eeb5800bb0efcaa65e33c
no name <noemail@gmail.com>
2024-03-05 16:14:01 +0800

    456

---
+++ assets/UserIndex-2FCaChFo.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-TAAB46RF.js";import{V as s}from"./Vip-TvJwEreM.js";import"./useAuthNavi-1jCOrt3A.js";function t(){return r.jsx("div",{children:r.jsx("div",{className:"max-w-[6.3rem]",children:r.jsx(s,{})})})}export{t as default};
